0cf19a228dbe40e617a8af553126cccd1562497e
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2017-10-04 13:06:09 +0200

    Initial Commit

--- src/main/java/za/co/fynbos/excel/ExcelReadWriteApplication.java
+++ src/main/java/za/co/fynbos/excel/ExcelReadWriteApplication.java
@@ -1,6 +1,7 @@
 package za.co.fynbos.excel;
 
 import za.co.fynbos.excel.read.EmployeeExcelWrite;
+import za.co.fynbos.excel.write.EmployeeExcelRead;
 /**
  *
  * @author NMandisa Mkhungo
@@ -10,8 +11,8 @@ public class ExcelReadWriteApplication {
     private static final String FILE_NAME = System.getProperty("user.dir") + "/sample_data.xlsx";
 
     public static void main(String[] args) {
-        EmployeeExcelWrite employeeExcelWrite = new EmployeeExcelWrite(FILE_NAME);
-        
+        //EmployeeExcelWrite employeeExcelWrite = new EmployeeExcelWrite(FILE_NAME);
+        EmployeeExcelRead employeeExcelRead = new EmployeeExcelRead(FILE_NAME);
     }
 
 }
--- src/main/java/za/co/fynbos/excel/write/EmployeeExcelRead.java
+++ src/main/java/za/co/fynbos/excel/write/EmployeeExcelRead.java
@@ -1,9 +1,59 @@
 package za.co.fynbos.excel.write;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Iterator;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 /**
  *
  * @author NMandisa Mkhungo
  */
 public class EmployeeExcelRead {
-    
+
+    public EmployeeExcelRead(String filename) {
+
+        try {
+
+            FileInputStream excelFile = new FileInputStream(new File(filename));
+            Workbook workbook = new XSSFWorkbook(excelFile);
+
+            Sheet employeeDataSheet = workbook.getSheetAt(0);
+            Iterator<Row> iterator = employeeDataSheet.iterator();
+
+            while (iterator.hasNext()) {
+
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
+
+                while (cellIterator.hasNext()) {
+
+                    Cell currentCell = cellIterator.next();
+                    //getCellTypeEnum shown as deprecated for version 3.15
+                    //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
+                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
+                        System.out.print(currentCell.getStringCellValue() + " | ");
+                    } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+                        System.out.print(currentCell.getNumericCellValue() + " | ");
+                    }
+
+                }
+                System.out.println();
+
+            }
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+
+    }
+
 }
