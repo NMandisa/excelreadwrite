5b5b0ef65d97532d4780787923ed1bbdea3634c3
Noxolo Mandisa Mkhungo <noxolomandisamkhungo@gmail.com>
2017-10-04 13:15:13 +0200

    Initial Commit

--- src/main/java/za/co/fynbos/excel/write/EmployeeExcelRead.java
+++ src/main/java/za/co/fynbos/excel/write/EmployeeExcelRead.java
@@ -36,8 +36,6 @@ public class EmployeeExcelRead {
                 while (cellIterator.hasNext()) {
 
                     Cell currentCell = cellIterator.next();
-                    //getCellTypeEnum shown as deprecated for version 3.15
-                    //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
                     if (currentCell.getCellTypeEnum() == CellType.STRING) {
                         System.out.print(currentCell.getStringCellValue() + " | ");
                     } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
